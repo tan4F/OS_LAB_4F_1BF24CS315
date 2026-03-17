e3ef383ea32bf9502c98d079d9ce6e2e9c61053d
tan4F <tanishkkar.cs24@bmsce.ac.in>
2026-03-17 20:23:46 +0530

    6-3-2026

--- FCFS/fcfs.c
+++ FCFS/fcfs.c
@@ -1,62 +1,70 @@
 #include <stdio.h>
-//FCFS Algorithm
-//Input - List of processes (PID, AT, BT)
-//Output - CT, TAT, WT
 
+int main() {
+    int n, i, j;
 
-
- struct Process{
-        int pid;
-        int at;
-        int bt;
-        int ct;
-        int tat;
-        int wt;
-    };
-
-void main(){
-    int n, i,j;
-    struct Process proc[10], temp;
-    int curTime = 0;
     printf("Enter number of processes: ");
     scanf("%d", &n);
-    float avgTime=0.0, waitTime=0.0;
-    for (i=0;i<n;i++){
-        printf("Enter Arrival and Burst Times for P%d: ", i+1);
-        proc[i].pid = i+1;
-        scanf("%d %d", &proc[i].at, &proc[i].bt);
+
+    int pid[n], at[n], bt[n], ct[n], tat[n], wt[n];
+    int current_time = 0;
+    float avg_tat = 0, avg_wt = 0;
+
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter Process ID for process %d: ", i+1);
+        scanf("%d", &pid[i]);
+
+        printf("Enter Arrival Time for process %d: ", i+1);
+        scanf("%d", &at[i]);
+
+        printf("Enter Burst Time for process %d: ", i+1);
+        scanf("%d", &bt[i]);
     }
 
-    for(i=0; i<n-1; i++){
-        for(j=0; j<n-i-1; j++){
-                if(proc[j].at>proc[j+1].at){
-                    temp = proc[j];
-                    proc[j]=proc[j+1];
-                    proc[j+1]=temp;
-                    proc[j+1]=temp;
+    for(i = 0; i < n-1; i++)
+    {
+        for(j = i+1; j < n; j++)
+        {
+            if(at[i] > at[j])
+            {
+                int temp;
+
+                temp = at[i]; at[i] = at[j]; at[j] = temp;
+                temp = bt[i]; bt[i] = bt[j]; bt[j] = temp;
+                temp = pid[i]; pid[i] = pid[j]; pid[j] = temp;
+            }
         }
     }
-}
+
+    for(i = 0; i < n; i++)
+    {
+        if(current_time < at[i])
+            current_time = at[i];
+
+        ct[i] = current_time + bt[i];
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+
+        current_time = ct[i];
+
+        avg_tat += tat[i];
+        avg_wt += wt[i];
+    }
 
     printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
-    for(i=0; i<n; i++){
-        if(curTime < proc[i].at) {
-            curTime = proc[i].at;
-        }
 
-        proc[i].ct = curTime + proc[i].bt;
-        proc[i].tat = proc[i].ct - proc[i].at;
-        proc[i].wt = proc[i].tat - proc[i].bt;
-        curTime = proc[i].ct;
-
-        avgTime += proc[i].tat;
-        waitTime += proc[i].wt;
-
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", proc[i].pid, proc[i].at, proc[i].bt, proc[i].ct, proc[i].tat, proc[i].wt);
+    for(i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n",
+        pid[i], at[i], bt[i], ct[i], tat[i], wt[i]);
     }
 
+    avg_tat /= n;
+    avg_wt /= n;
 
+    printf("\nAverage Turnaround Time = %.2f", avg_tat);
+    printf("\nAverage Waiting Time = %.2f\n", avg_wt);
 
-    printf("\nAvg Waiting Time: %.2f", waitTime/ n);
-    printf("\nAvg Turnaround Time: %.2f\n", avgTime / n);
+    return 0;
 }
